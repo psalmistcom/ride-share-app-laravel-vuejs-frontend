00beaf90555533f74dd0b23cb28bce6ee9ea4a1d
psalmistcom <dadewale9@gmail.com>
2023-09-12 18:51:46 +0100

    login api and rerouting

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="pt-16">
         <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-        <form action="#" @submit.prevent="handleLogin">
+        <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
@@ -16,37 +16,77 @@
                 </div>
             </div>
         </form>
-        <!-- <form action="#">
+        <form v-else action="#" @submit.prevent="handleVerification">
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
-                        <input type="text" name="login_code" id="login_code" placeholder="123456"
+                        <input type="text" v-maska data-maska="######" v-model="credentials.login_code" name="phone"
+                            id="login_code" placeholder="123456"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button type="submit"
-                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Verify</button>
+                    <button type="submit" @submit.prevent="handleVerification"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Continue</button>
                 </div>
             </div>
-        </form> -->
+        </form>
+
     </div>
 </template>
 
 <script setup>
 import { vMaska } from 'maska'
-import { reactive } from 'vue'
+import { computed, onMounted, reactive, ref } from 'vue'
 import axios from 'axios'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
 
 const credentials = reactive({
-    phone: null
+    phone: null,
+    login_code: null
 })
+
+const waitingOnVerification = ref(false)
+
+onMounted(() => {
+    if (localStorage.getItem('token')) {
+        router.push({
+            name: 'landing'
+        })
+    }
+})
+
+const getFormattedCredentials = () => {
+    return {
+        phone: credentials.phone.replaceAll(' ', '').replace('(', '').replace(')', '').replace('-', ''),
+        login_code: credentials.login_code
+    }
+}
+
 const handleLogin = () => {
-    axios.post('http://localhost:8000/api/login', credentials)
+    axios.post('http://localhost:8000/api/login', getFormattedCredentials())
         .then((response) => {
             console.log(response.data)
+            waitingOnVerification.value = true
         })
         .catch((error) => {
+            console.error(error)
+            alert(error.response.data.message)
+        })
+}
+
+const handleVerification = () => {
+    axios.post('http://localhost:8000/api/login/verify', getFormattedCredentials())
+        .then((response) => {
+            console.log(response.data)
+            localStorage.setItem('token', response.data)
+            router.push({
+                name: 'landing'
+            })
+        }).catch((error) => {
             console.error(error)
             alert(error.response.data.message)
         })
