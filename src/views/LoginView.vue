bab9744e171370ea37f3ad65cea5d5aa605e0753
psalmistcom <dadewale9@gmail.com>
2023-09-12 15:57:49 +0100

    fiixes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="pt-16">
         <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-        <form action="#">
+        <form action="#" @submit.prevent="handleLogin">
             <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
                 <div class="bg-white px-4 py-5 sm:p-6">
                     <div>
-                        <input type="text" v-maska data-maska="# (###) ###-####" name="phone" id="phone"
-                            placeholder="1 (234) 567-8910"
+                        <input type="text" v-maska data-maska="(###) #### ### ###" v-model="credentials.phone" name="phone"
+                            id="phone" placeholder="(234) 8067 891 010"
                             class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none">
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button type="submit"
+                    <button type="submit" @submit.prevent="handleLogin"
                         class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Continue</button>
                 </div>
             </div>
@@ -35,5 +35,21 @@
 
 <script setup>
 import { vMaska } from 'maska'
+import { reactive } from 'vue'
+import axios from 'axios'
+
+const credentials = reactive({
+    phone: null
+})
+const handleLogin = () => {
+    axios.post('http://localhost:8000/api/login', credentials)
+        .then((response) => {
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.error(error)
+            alert(error.response.data.message)
+        })
+}
 </script>
 <style></style>
